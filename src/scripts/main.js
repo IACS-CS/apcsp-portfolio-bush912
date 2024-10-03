b49761086034c9033442f5a0372a39eed871bfc8
bush912 <145694811+bush912@users.noreply.github.com>
2024-10-03 13:25:35 +0000

    instructions complete

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,12 +1,16 @@
 import "../styles/styles.css";
-// Add imports here
+"@picocss/pico/css/pico.min.css";
+import Aos from "aos";
+import "aos/dist/aos.css";
 
 console.log("What?");
 console.log("Hello world, from main.js!");
 if (typeof window !== "undefined") {
-  // Put code that runs ONLY in the browser here -- this is most likely where
+  // Put code that runs ONLY in the browser here
+  console.log("Hello world, from main.js!");
   // MOST of your code should go.
   //
+  Aos.init()
   // If you see a SSR error, try moving your code here!
   console.log("Hello world, browser :-)");
 }
