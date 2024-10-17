67cbb727b4ebccbb6a8c53844d11d46dc86e459d
bush912 <145694811+bush912@users.noreply.github.com>
2024-10-17 22:13:24 +0000

    changes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,16 +1,31 @@
 import "../styles/styles.css";
-"@picocss/pico/css/pico.min.css";
+//import "@picocss/pico/css/pico.min.css";
 import Aos from "aos";
 import "aos/dist/aos.css";
 
+import Particles from "particlesjs";
+console.log("Imported", Particles);
+var particles = Particles.init({
+  selector: ".background",
+  color: ["#DA0463", "#404B69", "pink", "orange", "teal", "#0033a0"],
+  connectParticles: false,
+  sizeVariation: 1000,
+  maxParticles: 1000,
+  responsive: [
+    {
+      breakpoint: 800,
+      options: {
+        color: "#00C9B1",
+        maxParticles: 80,
+        connectParticles: false,
+      },
+    },
+  ],
+});
+
+console.log("Running?", particles);
 console.log("What?");
 console.log("Hello world, from main.js!");
-if (typeof window !== "undefined") {
-  // Put code that runs ONLY in the browser here
-  console.log("Hello world, from main.js!");
-  // MOST of your code should go.
-  //
-  Aos.init()
-  // If you see a SSR error, try moving your code here!
-  console.log("Hello world, browser :-)");
-}
+
+Aos.init()
+ 
